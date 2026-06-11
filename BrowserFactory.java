29420ddf06f908f61914ecc224213d603b3f74e8
Debjit Saha <124062703+Debjit-cmd@users.noreply.github.com>
2026-06-12 01:11:54 +0530

    Add files via upload

--- BrowserFactory.java
+++ BrowserFactory.java
@@ -6,20 +6,18 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BrowserFactory {
-   static WebDriver driver;
-   
-   public static WebDriver BrowserOptions(String browserName,String url) {
-	   if(browserName.equalsIgnoreCase("chrome")) {
-		   driver = new ChromeDriver();
-	   }
-	   else if(browserName.equalsIgnoreCase("edge")) {
-		   driver = new EdgeDriver();
-	   }
-	   else if(browserName.equalsIgnoreCase("firefox")) {
-		   driver = new FirefoxDriver();
-	   }
-	   driver.manage().window().maximize();
-	   driver.get(url);
-	   return driver;
-   }
-} 
+	static WebDriver driver;
+
+	public static WebDriver BrowserOptions(String browserName, String url) {
+		if (browserName.equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver();
+		} else if (browserName.equalsIgnoreCase("edge")) {
+			driver = new EdgeDriver();
+		} else if (browserName.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		}
+		driver.manage().window().maximize();
+		driver.get(url);
+		return driver;
+	}
+}
--- ExecuteLoginPage.java
+++ ExecuteLoginPage.java
@@ -1,18 +1,25 @@
-package com.EasycalculationPages;
+package com.ChaseTestCases;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
-import com.Easycalculation.TestCases.LoginEasyCal;
+import com.ChasePOMPages.PomChaseLoginPage;
 import com.Helper.BrowserFactory;
 
 public class ExecuteLoginPage {
-	@Test
-   public void CheckValidUser() {
-	   WebDriver driver = BrowserFactory.BrowserOptions("chrome", "https://www.login.hiox.com/login?referrer=easycalculation.com");
-	   LoginEasyCal logineasycal = PageFactory.initElements(driver, LoginEasyCal.class);
-	   logineasycal.Login_Easy_Cal("8373838333", "dnwdbqd123@");
-	   
-   }
+
+    @Test
+    public void CheckValidUser() throws InterruptedException 
+    {
+
+        WebDriver driver = BrowserFactory.BrowserOptions("chrome", "https://www.chase.com/");
+
+        PomChaseLoginPage page =
+                PageFactory.initElements(driver, PomChaseLoginPage.class);
+        
+        Thread.sleep(5000);
+        
+        page.LoginEasyCal("student","Password123");
+    }
 }
